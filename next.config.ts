053037083bcff64bc39e2c6c33e5b5b9830975f7
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-09-08 16:41:55 +0000

    agrega el dominio de youtube para que deje reproducir el video

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
