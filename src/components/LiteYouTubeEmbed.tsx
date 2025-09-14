8f9fd743d8fe0764dcf05822eca8ef9d4c35813c
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-09-14 11:55:46 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/LiteYouTubeEmbed.tsx
+++ src/components/LiteYouTubeEmbed.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, 'useState' from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { PlayCircle } from 'lucide-react';
 import { cn } from '@/lib/utils';
