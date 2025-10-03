924a8d884350b8419a9bdaa8ed7b7cfcf4a31601
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-03 12:11:57 +0000

    vale oeri si hacemos la pagina dimamica se puede ?

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
