4390726a2863bbb586f510badd072fa8e86d7c96
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-04 22:16:41 +0000

    ok mira actualiza tu blueprint el nuevo objetivo es crear un ecomerce pa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription, SheetHeader, SheetFooter } from '@/components/ui/sheet';
