e400b96182d92d893e7d9865ab146129b7157146
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-08 14:38:33 +0000

    hasla un 50 % mas pequeña en el footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
               </div>
             </div>
             <div className="flex flex-col items-center">
-               <Image src="/image/Logo_mortys_cake.webp" alt="Pastelería de Morty" width={180} height={64} className="object-contain h-auto w-auto" data-ai-hint="logo" />
+               <Image src="/image/Logo_mortys_cake.webp" alt="Pastelería de Morty" width={90} height={32} className="object-contain h-auto w-auto" data-ai-hint="logo" />
             </div>
             <div className="flex flex-col items-end">
               <h3 className="font-bold text-lg mb-4 font-headline">Síguenos</h3>
