6678a147bef003c6a1b5b83a98aacade4b50119b
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-08 16:35:47 +0000

    usa este para el favicon src/app/icon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,11 +75,6 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  }
 };
 
 export const viewport: Viewport = {
