5602daa5433b9440045bfe92c38267074db6c426
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-08 00:00:53 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- wordpress-functions.php
+++ wordpress-functions.php
@@ -18,6 +18,8 @@ add_action( 'rest_api_init', function() {
     remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
     add_filter( 'rest_pre_serve_request', function( $value ) {
         header( 'Access-Control-Allow-Origin: *' );
+        header( 'Access-Control-Allow-Methods: GET' );
+        header( 'Access-Control-Allow-Headers: Content-Type' );
         return $value;
     });
 }, 15 );
