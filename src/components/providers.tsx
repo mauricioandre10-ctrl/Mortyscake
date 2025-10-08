b1df22c16dc7526910e6fe209cc6c4b9f239cc30
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-08 22:19:46 +0000

    [{ 	"resource": "/home/user/studio/src/components/providers.tsx", 	"owne

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -7,7 +7,7 @@ const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <USCProvider
-      cartMode="payment"
+      cartMode="client-only"
       stripe={stripePublicKey!}
       successUrl={`${process.env.NEXT_PUBLIC_SITE_URL || ''}/?success=true`}
       cancelUrl={`${process.env.NEXT_PUBLIC_SITE_URL || ''}/?canceled=true`}
