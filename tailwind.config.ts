97b4763693ad1075c768482cf6c8113a98a86dbd
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-09 14:00:13 +0000

    vale pero la tencicion es muy rapida has que sea mas lento que duren com

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,7 +98,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in': 'fade-in 0.5s ease-in-out',
+        'fade-in': 'fade-in 2s ease-in-out',
       },
     },
   },
