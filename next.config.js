f658a3f6294476eadcd42817bb7c2474262d042e
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-09 19:19:57 +0000

    agrega la seguridad de encabezados Content-Security-Policy	Content Secu

--- next.config.js
+++ next.config.js
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
+
+// Define la Content Security Policy (CSP)
+const ContentSecurityPolicy = `
+  default-src 'self' https://*.mortyscake.es https://*.google.com https://www.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.youtube.com;
+  child-src 'self' https://www.youtube.com https://www.youtube-nocookie.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  img-src 'self' data: https:;
+  font-src 'self' https://fonts.gstatic.com;
+  connect-src 'self' https://*.mortyscake.es https://www.google-analytics.com;
+  frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com;
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), payment=()'
+  }
+];
+
 const nextConfig = {
   /* config options here */
   typescript: {
@@ -6,6 +42,14 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
   images: {
     remotePatterns: [
@@ -26,6 +70,12 @@ const nextConfig = {
         hostname: 'secure.gravatar.com',
         port: '',
         pathname: '/avatar/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
       }
     ],
   },
