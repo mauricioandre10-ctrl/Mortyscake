a07ad4cd913a68f2dbd09e972bbfae35e97a8088
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-09 13:10:56 +0000

    no funciono asi se ve ahora

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -66,24 +66,22 @@ export default function GalleryPage() {
         </div>
         
         {selectedImage && (
-             <DialogContent className="max-w-[95vw] w-auto h-auto p-0 bg-transparent border-0 flex items-center justify-center">
-                <div className="relative w-auto h-auto max-h-[90vh]">
-                    <Image 
-                        src={selectedImage.src}
-                        alt={selectedImage.alt}
-                        width={selectedImage.width}
-                        height={selectedImage.height}
-                        className="object-contain h-auto w-auto max-h-[90vh] max-w-[95vw] rounded-lg shadow-2xl"
-                        sizes="95vw"
-                    />
-                     <ShareButton
-                        title="Mira esta creación de Morty's Cake"
-                        text="¡Me encantó esta foto de la galería de Morty's Cake!"
-                        url={siteUrl ? `${siteUrl}${selectedImage.src}` : selectedImage.src}
-                        className="absolute top-2 right-2 z-10 bg-black/50 text-white hover:bg-black/70"
-                        size="icon"
-                     />
-                </div>
+             <DialogContent className="max-w-[95vw] w-auto h-auto p-2 bg-transparent border-0 flex items-center justify-center">
+                <Image 
+                    src={selectedImage.src}
+                    alt={selectedImage.alt}
+                    width={selectedImage.width}
+                    height={selectedImage.height}
+                    className="object-contain h-auto w-auto max-h-[90vh] max-w-[95vw] rounded-lg shadow-2xl"
+                    sizes="95vw"
+                />
+                 <ShareButton
+                    title="Mira esta creación de Morty's Cake"
+                    text="¡Me encantó esta foto de la galería de Morty's Cake!"
+                    url={siteUrl ? `${siteUrl}${selectedImage.src}` : selectedImage.src}
+                    className="absolute top-4 right-4 z-20 bg-black/50 text-white hover:bg-black/70"
+                    size="icon"
+                 />
             </DialogContent>
         )}
       </Dialog>
