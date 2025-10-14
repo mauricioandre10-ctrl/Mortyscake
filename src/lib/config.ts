fae10eb7c8a6ee0563a062d9018f430ae7a23c2b
Mauricio Sanchez <mauricioandre10@hotmail.com>
2025-10-14 20:19:51 +0000

    estas son las variables de entorno que tengo en vercel son iguales a las

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,5 +2,5 @@
 
 // Lee la variable de entorno una sola vez y la exporta para ser usada en toda la aplicación.
 // Esto asegura que Next.js la procese correctamente y la incluya en el bundle del cliente.
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+export const apiUrl = process.env.NEXT_PUBLIC_WOOCOMMERCE_STORE_URL;
 export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
